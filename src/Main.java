22aab72af86970923d58c6c9dd26ead43fe6c08e
alexeibob1 <94921872+alexeibob1@users.noreply.github.com>
2021-10-31 22:28:31 +0300

    Attempt

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
  public class Main {
 	 public static void main (String[] args) {
 		 System.out.println("I'm just checking how it's working");
-		 System.out.println("I'm just checking how it's working"); //test
+		 System.out.println("I'm just checking how it's working");
 	 }
 }
