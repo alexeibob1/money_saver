0972b16b3702579db09e77b2ee21bad898f5b37c
alexeibob1 <94921872+alexeibob1@users.noreply.github.com>
2021-10-31 22:20:47 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
  public class Main {
 	 public static void main (String[] args) {
 		 System.out.println("I'm just checking how it's working");
-		 System.out.println("I'm just checking how it's working");
-		 System.out.println("I'm just checking how it's working");
 	 }
 }
