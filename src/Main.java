3ecd87b8c90d9ce006896543cc7c9c19c5758a5d
bobriki <bobriki@LAPTOP-IV97264L>
2021-11-23 23:43:07 +0300

    Update main
    I believe in God

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@
 	 public static void main (String[] args) {
 		 System.out.println("I'm just checking how it's working");
 		 System.out.println("I'm just checking how it's working");
+		 System.out.println("I'm just checking how it's working");
 	 }
 }
