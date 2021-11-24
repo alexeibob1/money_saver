21a82f296f518ac94876c9e272b7461130c511f9
Ptuxa <sdnaft@gmail.com>
2021-11-24 22:00:47 +0300

    Добавлена функция проверки внесённой пользователем суммы денежных средств на корректность.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,77 +7,71 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 /**
- * this is the central class in which the body of the program is located
- * */
-class ExpenseAccounting {
-	/**
-	 * import java.util.Scanner to get information from the console.
-	 */
+ * Подключение к БД Microsoft Access (accdb) используя драйвер UCanAccess JDBC
+ * Установка драйвера UCanAccess в Intellij IDEA
+ * https://www.youtube.com/watch?v=xM1KNbRkF3A
+ */
+public class ExpenseAccounting {
 	public static Scanner scanner = new Scanner(System.in);
-	/**
-	 * this is the central method inside it, other methods are created and used.
-	 * */
+
 	public static void main(String[] args) {
 
-		//Connection with DB MS ACCESS. Using drawer UCanAccess. Input correct path to our DB (.accdb)
+		//Подключение к БД MS ACCESS. Используем драйвер UCanAccess. Указан полный путь к файлу БД (.accdb)
 		String databaseURL = "jdbc:ucanaccess://d://Database_Accounting.accdb";
 		int userChoice = -1;
 		boolean closeProg = false;
-		String menuMsg =
-				"\nг====================================¬" +
-						"\n¦                Menu                ¦" +
-						"\n¦===T================================¦" +
-						"\n¦ 1 ¦ Create account                 ¦" +
-						"\n¦ 2 ¦ Create category                ¦" +
-						"\n¦ 3 ¦ Top up account                 ¦" +
-						"\n¦ 4 ¦ Enter expense on the account   ¦" +
-						"\n¦ 5 ¦ Operations history             ¦" +
-						"\n¦ 6 ¦ Delete account                 ¦" +
-						"\n¦ 7 ¦ Delete category                ¦" +
-						"\n¦ 8 ¦ Delete operation               ¦" +
-						"\n¦ 9 ¦ Category statistics            ¦" +
-						"\n¦===+================================¦" +
-						"\n¦ 0 ¦ Exit                           ¦" +
-						"\nL===¦================================-" +
-						"\nSelect Menu item: ";
-
-
+		String menuMsg = "\n╔════════════════════════════════════╗" +
+				"\n║                Меню                ║" +
+				"\n╠═══╦════════════════════════════════╣" +
+				"\n║ 1 ║ Создать счет                   ║" +
+				"\n║ 2 ║ Создать категорию              ║" +
+				"\n║ 3 ║ Пополнить счет                 ║" +
+				"\n║ 4 ║ Внести расход средств по счету ║" +
+				"\n║ 5 ║ История операций               ║" +
+				"\n║ 6 ║ Удалить счет                   ║" +
+				"\n║ 7 ║ Удалить категорию              ║" +
+				"\n║ 8 ║ Удалить операцию               ║" +
+				"\n║ 9 ║ Статистика по категориям       ║" +
+				"\n╠═══╬════════════════════════════════╣" +
+				"\n║ 0 ║ Выйти                          ║" +
+				"\n╚═══╩════════════════════════════════╝" +
+				"\nВыберите пункт Меню: ";
 		do {
 			try {
 				Connection connection = DriverManager.getConnection(databaseURL);
 				userChoice = checkMenuUserChoice(connection, "mainmenu", menuMsg);
 
 				switch (userChoice) {
-
+					//Создать счет
 					case 1 -> addAccount(connection);
-
+					//Создать категорию
 					case 2 -> addCategory(connection);
-
+					//Пополнить счет
 					case 3 -> addActivity2Account(connection, 1);
-
+					//Внести расход средств по счету
 					case 4 -> addActivity2Account(connection, -1);
-
+					//История операций
 					case 5 -> outputAccountActivity(connection, -1, 5);
-
+					//Удалить счет
 					case 6 -> {
 						deleteAccount(connection);
 						outputAllAccountsInfo(connection);
 					}
-
+					//Удалить категорию
 					case 7 -> {
 						deleteCategory(connection);
 						outputAllAccountsInfo(connection);
 					}
-
+					//Удалить операцию
 					case 8 -> {
 						deleteAccountActivity(connection, -1);
 						outputAllAccountsInfo(connection);
 					}
-
+					//Статистика по категориям
 					case 9 -> categoryStatistics(connection);
-
+					//выход из программы
 					case 0 -> closeProg = true;
-
+					//если пользователь сделал некорректный выбор
 					default -> {
 						System.err.println("Некорректный ввод! Введите число из списка!");
 						closeProg = false;
@@ -85,8 +79,7 @@ class ExpenseAccounting {
 				}
 
 				//   connection.close();
-			}
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				//вывести сообщение ошибке в консоль
 				System.err.println("При работе с базой данных произошла ошибка! Проверьте информацию в логе и состояние базы данных");
 				System.err.println("Error Message: " + e.getMessage());
@@ -98,7 +91,7 @@ class ExpenseAccounting {
 				closeProg = true; //выйти из программы
 			}
 
-			if (userChoice !=0 ) {
+			if (userChoice != 0) {
 				closeProg = back2menu();
 			}
 
@@ -108,14 +101,7 @@ class ExpenseAccounting {
 
 	}
 
-	/**
-	 * This method check user input
-	 * @param connection it is the link between project and DB
-	 * @param menuType it if our work space (account, category and other)
-	 * @param msg contains menu of this project
-	 * @throws SQLException in case of errors in working with DB.
-	 * @return correct user input else ask new value
-	 */
+	//Проверка пользовательского ввода
 	static int checkMenuUserChoice(Connection connection, String menuType, String msg) throws SQLException {
 		boolean isCorrect = false;
 		Integer choice = null;
@@ -133,10 +119,7 @@ class ExpenseAccounting {
 		return choice;
 	}
 
-	/**
-	 * This method check what user want exit the program or continue working
-	 * @return false if user want to stop work(input 1), true if user want to continue work(input 0), else ask new value
-	 */
+	//Возврат в основное меню (1) или выход (0)
 	static boolean back2menu() {
 		Integer backMenuChoice = null;
 		boolean isCorrect = false;
@@ -168,12 +151,8 @@ class ExpenseAccounting {
 		return rezult;
 	}
 
-	/**
-	 * This method check user input, integer  this input or not
-	 * @param consoleInput checking value
-	 * @return start value if it is integer else changes it to -1
-	 */
-	public static int checkIntOrNot(String consoleInput) {
+	//Проверка данных введенных пользователем (integer or not)
+	static int checkIntOrNot(String consoleInput) {
 		Integer choice = null;
 		try {
 			choice = Integer.parseInt(consoleInput);
@@ -183,12 +162,8 @@ class ExpenseAccounting {
 		return choice;
 	}
 
-	/**
-	 * This method check user input, double this input or not
-	 * @param consoleInput checking value
-	 * @return true if checking value is correct else return false
-	 * */
-	public static boolean checkDoubleOrNot(String consoleInput) {
+	//Проверка суммы введенной пользователем (double or not)
+	static boolean checkDoubleOrNot(String consoleInput) {
 		boolean isCorrect = false;
 		try {
 			double userSum = Double.parseDouble(consoleInput);
@@ -196,16 +171,27 @@ class ExpenseAccounting {
 		} catch (NumberFormatException e) {
 			isCorrect = false;
 		}
-		return  isCorrect;
+		return isCorrect;
 	}
 
-	/**
-	 * this method calculates new balance amount
-	 * @param currentBalance this is the start amount on the balance
-	 * @param transactionSum this is the sum of the balance change.
-	 * @param flag shows what we do increase balance or decrease.
-	 * @return this method returns the changed balance
-	 * */
-	static double countSum(double currentBalance, double transactionSum, int flag ) {
-		return  Math.round((Math.abs(currentBalance) + Math.abs(transactionSum) * flag) * 100) / 100.0;
+	//Подсчет суммы
+	static double countSum(double currentBalance, double transactionSum, int flag) {
+		return Math.round((Math.abs(currentBalance) + Math.abs(transactionSum) * flag) * 100) / 100.0;
 	}
+
+	//Проверка пользовательского ввода суммы
+	static double checkTransactionAmount() {
+		double transactionAmount = 0d;
+		boolean isCorrect = false;
+		while (!isCorrect) {
+			String consoleInput = scanner.nextLine();
+			isCorrect = checkDoubleOrNot(consoleInput);
+			if (isCorrect) {
+				transactionAmount = Double.parseDouble(consoleInput);
+			} else {
+				System.err.print("Некорректный ввод суммы! Используйте формат (рублей.копеек): ");
+			}
+		}
+		return Math.round(transactionAmount * 100.0) / 100.0;
+	}
+}
